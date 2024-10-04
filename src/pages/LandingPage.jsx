df29cf534096d08eb27358a7f23de804c79888ec
Mario Fragnito <mariofragnitoph@gmail.com>
2024-10-04 16:30:51 +0200

    Adds landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,5 +1,6 @@
 import VideoBg from "reactjs-videobg/dist/bundle.cjs";
 import video from "../assets/video-bg.mp4";
+import { Container } from "react-bootstrap";
 
 const LandingPage = () => {
   return (
@@ -7,6 +8,22 @@ const LandingPage = () => {
       <VideoBg>
         <VideoBg.Source src={video} type="video/mp4" />
       </VideoBg>
+      <Container>
+        <div className="hero d-flex justify-content-center align-items-center flex-column p-3 ">
+          <h1 className="header">
+            Game <span className="text-secondary">Vault</span>
+          </h1>
+          <p className="h2">
+            Your game library on the <span className="text-secondary">go.</span>
+          </p>
+          <p className="h5">
+            Already signed up? <span className="text-secondary">Login</span>
+          </p>
+          <p className="h5">
+            Need an account? <span className="text-secondary">Sign up</span>
+          </p>
+        </div>
+      </Container>
     </>
   );
 };
