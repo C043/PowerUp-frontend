d77c8520435e4706541db04500944897bd5f1d90
C043 <mariofragnitoph@gmail.com>
2024-10-06 13:21:55 +0200

    WIP SideBar

--- src/components/sideBar/SideBar.jsx
+++ src/components/sideBar/SideBar.jsx
@@ -2,7 +2,12 @@ import "./SideBar.scss"
 
 const SideBar = () => {
   return <div className="sideBar me-5 mt-4 position-sticky">
-    <p className="h3">Filters</p>
+    <p className="h2">Filters</p>
+    <p className="h6">Platforms</p>
+    <p className="h6">Genres</p>
+    <p className="h2">Order</p>
+    <p className="h6">Title</p>
+    <p className="h6">Genre</p>
   </div>
 }
 
