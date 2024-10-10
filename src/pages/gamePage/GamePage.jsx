802a5f973a3ff82df1766cc8ea4b2a7aea38cebc
Mario Fragnito <mariofragnitoph@gmail.com>
2024-10-10 14:48:09 +0100

    Adds game fetchAdds game fetch

--- src/pages/gamePage/GamePage.jsx
+++ src/pages/gamePage/GamePage.jsx
@@ -1,7 +1,34 @@
 import { Container } from "react-bootstrap"
 import NavBar from "../../components/navBar/NavBar"
+import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
 
 const GamePage = () => {
+	const params = useParams()
+	const token = localStorage.getItem("token")
+	const [game, setGame] = useState()
+
+	const fetchGame = async () => {
+		try {
+			const resp = await fetch(`http://localhost:3001/games/${params.gameId}`, {
+				headers: {
+					"Authorization": "Bearer " + token
+				}
+			})
+			if (resp.ok) {
+				const data = await resp.json()
+				setGame(data)
+				console.log(game)
+			} else throw new Error("Fetch Error")
+		} catch (error) {
+			console.log(error.message)
+		}
+	}
+
+	useEffect(() => {
+		fetchGame()
+	}, [])
+
 	return <>
 		<NavBar />
 		<Container>
