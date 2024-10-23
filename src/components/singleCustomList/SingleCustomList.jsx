37be5ee6279224234d33df4538dd9e263eab8cb6
C043 <mariofragnitoph@gmail.com>
2024-10-23 12:30:16 +0200

    Edited single custom list component

--- src/components/singleCustomList/SingleCustomList.jsx
+++ src/components/singleCustomList/SingleCustomList.jsx
@@ -5,11 +5,13 @@ const SingleCustomList = ({ title, listId }) => {
   const navigate = useNavigate()
 
   return <div
-    className="singleCustomList d-flex flex-column justify-content-center rounded align-items-center"
+    className="singleCustomList d-flex flex-column align-items-center rounded"
     onClick={() => navigate(`/customList/${title}/${listId}`)}
     role="button"
   >
-    <p className="truncate my-3 h3">{title}</p>
+    <div className="blur w-100 h-100 d-flex justify-content-center rounded">
+      <p className="truncate my-3 h3">{title}</p>
+    </div>
   </div>
 }
 
