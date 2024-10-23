ec2b1d0809cf4cf7c267e286961a289f9821adce
C043 <mariofragnitoph@gmail.com>
2024-10-23 11:48:58 +0200

    WIP

--- src/components/singleCustomList/SingleCustomList.jsx
+++ src/components/singleCustomList/SingleCustomList.jsx
@@ -1,14 +1,15 @@
+import "./SingleCustomList.scss"
 import { useNavigate } from "react-router-dom"
 
 const SingleCustomList = ({ title, listId }) => {
   const navigate = useNavigate()
 
   return <div
-    className="d-flex flex-column justify-content-center bg-secondary rounded align-items-center"
+    className="singleCustomList d-flex flex-column justify-content-center rounded align-items-center"
     onClick={() => navigate(`/customList/${title}/${listId}`)}
     role="button"
   >
-    <p className="truncate my-3">{title}</p>
+    <p className="truncate my-3 h3">{title}</p>
   </div>
 }
 
